541b16c714e189fd3aaeec8e709c29840a67d664
@rickhogenhout <2385-rickhogenhout@users.noreply.bitlab.bit-academy.nl>
2023-11-27 11:17:33 +0100

    initial commit

---
+++ DATABASE/customers.sql
@@ -0,0 +1,40 @@
+
+    INSERT INTO customers (id, name, email)
+    VALUES (1, 'John Doe', 'jane_smith@example.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (2, 'John Doe', 'jane_smith@hotmail.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (3, 'Jane Smith', 'emily_davis@yahoo.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (4, 'Bob Johnson', 'bob_johnson@gmail.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (5, 'Bob Johnson', 'john_doe@example.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (6, 'Michael Wilson', 'emily_davis@yahoo.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (7, 'Emily Davis', 'emily_davis@example.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (8, 'Jane Smith', 'emily_davis@outlook.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (9, 'Emily Davis', 'bob_johnson@hotmail.com');
+    
+
+    INSERT INTO customers (id, name, email)
+    VALUES (10, 'John Doe', 'john_doe@yahoo.com');
+    
---
+++ DATABASE/reservations.sql
@@ -0,0 +1,250 @@
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (1, 3, 224,
+    3, '2023-12-01', '2023-12-12');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (2, 8, 144,
+    4, '2023-11-29', '2023-11-30');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (3, 18, 247,
+    1, '2023-11-30', '2023-12-02');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (4, 40, 150,
+    5, '2023-12-19', '2023-12-29');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (5, 41, 291,
+    2, '2023-11-30', '2023-12-11');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (6, 18, 303,
+    2, '2023-12-11', '2023-12-19');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (7, 13, 2,
+    4, '2023-12-10', '2023-12-15');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (8, 91, 192,
+    1, '2023-12-25', '2023-12-30');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (9, 46, 5,
+    2, '2023-12-08', '2023-12-22');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (10, 36, 402,
+    4, '2023-12-14', '2023-12-17');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (11, 49, 191,
+    3, '2023-11-30', '2023-12-03');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (12, 22, 294,
+    1, '2023-12-05', '2023-12-09');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (13, 18, 263,
+    3, '2023-12-20', '2023-12-23');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (14, 65, 288,
+    2, '2023-11-28', '2023-12-09');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (15, 62, 90,
+    5, '2023-12-09', '2023-12-23');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (16, 56, 158,
+    3, '2023-12-20', '2023-12-29');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (17, 73, 237,
+    3, '2023-12-14', '2023-12-18');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (18, 67, 216,
+    1, '2023-12-17', '2023-12-24');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (19, 40, 189,
+    1, '2023-12-25', '2024-01-01');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (20, 20, 221,
+    3, '2023-12-12', '2023-12-26');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (21, 7, 234,
+    3, '2023-12-26', '2024-01-03');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (22, 20, 236,
+    4, '2023-12-20', '2023-12-25');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (23, 84, 380,
+    5, '2023-12-05', '2023-12-10');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (24, 7, 264,
+    2, '2023-12-27', '2023-12-31');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (25, 58, 319,
+    2, '2023-12-22', '2023-12-31');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (26, 27, 162,
+    1, '2023-12-13', '2023-12-18');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (27, 33, 37,
+    3, '2023-12-03', '2023-12-15');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (28, 86, 144,
+    2, '2023-12-23', '2023-12-30');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (29, 36, 149,
+    3, '2023-12-24', '2023-12-29');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (30, 89, 398,
+    4, '2023-11-28', '2023-12-02');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (31, 32, 238,
+    1, '2023-12-09', '2023-12-19');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (32, 26, 405,
+    5, '2023-12-07', '2023-12-09');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (33, 100, 359,
+    3, '2023-12-06', '2023-12-11');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (34, 34, 374,
+    3, '2023-12-04', '2023-12-16');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (35, 86, 47,
+    5, '2023-12-13', '2023-12-25');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (36, 32, 108,
+    4, '2023-12-03', '2023-12-06');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (37, 81, 238,
+    3, '2023-12-23', '2024-01-01');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (38, 32, 383,
+    1, '2023-11-28', '2023-12-09');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (39, 1, 380,
+    4, '2023-12-08', '2023-12-09');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (40, 90, 93,
+    4, '2023-12-05', '2023-12-11');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (41, 41, 85,
+    5, '2023-12-23', '2023-12-31');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (42, 99, 343,
+    5, '2023-12-22', '2023-12-27');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (43, 89, 238,
+    3, '2023-12-15', '2023-12-25');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (44, 100, 225,
+    1, '2023-12-03', '2023-12-14');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (45, 93, 102,
+    3, '2023-12-12', '2023-12-20');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (46, 15, 46,
+    5, '2023-12-24', '2023-12-26');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (47, 82, 191,
+    4, '2023-12-06', '2023-12-19');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (48, 14, 259,
+    2, '2023-11-29', '2023-12-13');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (49, 50, 248,
+    1, '2023-12-25', '2023-12-26');
+    
+
+    INSERT INTO reservations (id, customer_id, property_id, manager_id, check_in, check_out)
+    VALUES (50, 71, 333,
+    5, '2023-12-06', '2023-12-14');
+    

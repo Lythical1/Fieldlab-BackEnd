2d84532e3697c61f19de3a38a101f5b80276cb8c
Fabian Nierop <165712@student.horizoncollege.nl>
2024-01-15 11:17:02 +0100

    update

--- api/index.php
+++ api/index.php
@@ -3,12 +3,15 @@
 // require all the files for the endpoints
 require_once 'properties.php';
 require_once 'customers.php';
+require_once 'bookings.php';
+require_once 'reviews.php';
 
 // Define a simple router
 $request_method = $_SERVER['REQUEST_METHOD'];
 $request_uri = $_SERVER['REQUEST_URI'];
 
 // properties endpoint
+echo $request_method . ' ' . $request_uri . PHP_EOL;
 if ($request_method === 'GET' && $request_uri === '/api/properties') {
     getProperties();
 } elseif ($request_method === 'GET' && $request_uri === '/api/customers') {
@@ -17,4 +20,12 @@ if ($request_method === 'GET' && $request_uri === '/api/properties') {
     // Handle invalid endpoint
     http_response_code(404);
     echo json_encode(['error' => 'Endpoint not found']);
+}
+
+if ($request_method === 'GET') {
+    try {
+        
+    } catch (\Throwable $th) {
+        //throw $th;
+    }
 }

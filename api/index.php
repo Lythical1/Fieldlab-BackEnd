f25a38557755a76b3d99d7db871d4f2f79cd70cb
Felix <felix.huel6@gmail.com>
2024-01-15 11:12:47 +0100

    recent api filter

--- api/index.php
+++ api/index.php
@@ -2,6 +2,7 @@
 
 // require all the files for the endpoints
 require_once 'properties.php';
+require_once 'customers.php';
 
 // Define a simple router
 $request_method = $_SERVER['REQUEST_METHOD'];
@@ -10,6 +11,8 @@ $request_uri = $_SERVER['REQUEST_URI'];
 // properties endpoint
 if ($request_method === 'GET' && $request_uri === '/api/properties') {
     getProperties();
+} elseif ($request_method === 'GET' && $request_uri === '/api/customers') {
+    getCustomers();
 } else {
     // Handle invalid endpoint
     http_response_code(404);
